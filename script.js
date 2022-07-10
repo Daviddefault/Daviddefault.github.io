cab4eee5515a1166f7c4fdddd27cd93673cb1952
Daviddefault <davidjy21@gmail.com>
2022-07-10 19:09:50 +0100

    dm

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
+const html = document.querySelector("html"); 
+
+html.classList.add("bg-dark");
+
+// darkmode switch
+document.querySelector("input").addEventListener("change", (event) => {
+	bgSwitch(event);
+});
+
+const bgSwitch = (event) => {
+  if (event.target.checked) {
+		html.classList.add("bg-dark");
+	} else {
+		html.classList.remove("bg-dark");
+	}
+}
+
+
+
 
 
 
 (function($) { "use strict";
 
-	$(function() {
-		var header = $(".start-style");
-		$(window).scroll(function() {    
-			var scroll = $(window).scrollTop();
-		
-			if (scroll >= 10) {
-				header.removeClass('start-style').addClass("scroll-on");
-			} else {
-				header.removeClass("scroll-on").addClass('start-style');
-			}
-		});
-	});		
-		
-	//Animation
+$(function() {
+	var header = $(".start-style");
+	$(window).scroll(function() {    
+		var scroll = $(window).scrollTop();
 	
-	$(document).ready(function() {
-		$('body.hero-anime').removeClass('hero-anime');
-	});
-
-	//Menu On Hover
-		
-	$('body').on('mouseenter mouseleave','.nav-item',function(e){
-			if ($(window).width() > 750) {
-				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-				setTimeout(function(){
-				_d[_d.is(':hover')?'addClass':'removeClass']('show');
-				},1);
-			}
-	});	
-	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("dark")) {
-			$("body").removeClass("dark");
-			$("#switch").removeClass("switched");
-			$("#switch").removeClass("dark");
-			$("#switch").removeClass("active");
+		if (scroll >= 10) {
+			header.removeClass('start-style').addClass("scroll-on");
+		} else {
+			header.removeClass("scroll-on").addClass('start-style');
 		}
-		else {
-			$("body").addClass("dark");
-			$("#switch").addClass("switched");
-			$("#switch").addClass("dark");
-			$("#switch").addClass("active");
-		};
+	});
+});		
+	
+//Animation
 
-	}); 
+$(document).ready(function() {
+	$('body.hero-anime').removeClass('hero-anime');
+});
 
-  })(jQuery);
+//Menu On Hover
+	
+$('body').on('mouseenter mouseleave','.nav-item',function(e){
+		if ($(window).width() > 750) {
+			var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+			setTimeout(function(){
+			_d[_d.is(':hover')?'addClass':'removeClass']('show');
+			},1);
+		}
+});	
+
+
+
+})(jQuery);
 
